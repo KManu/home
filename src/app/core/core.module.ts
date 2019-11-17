1914da9620b531e4344b6a56eaabc38a515ea787
Manu <kmanu7@teamcyst.com>
2019-11-17 08:28:47 +0000

    scaffold for home page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeModule } from '../pages/home/home.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent, HeaderComponent],
   imports: [
     CommonModule,
     HomeModule
+  ],
+  entryComponents: [FooterComponent, HeaderComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
