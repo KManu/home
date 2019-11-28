08b41ee12dcbc53d012f157d75a58e82350aff1d
Manu <kmanu7@teamcyst.com>
2019-11-28 00:26:39 +0000

    made frame responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import { Component, ChangeDetectorRef, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'home';
+export class AppComponent implements OnDestroy{
+  title = 'Home.';
+  mobileQuery: MediaQueryList;
+
+  private mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher){
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    /*  TODO: Change breakpoint detection method to use CDKs Breakpoint observers and breakpointstate
+    ref: https://alligator.io/angular/breakpoints-angular-cdk/
+    */
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
+
+  ngOnDestroy(){}
+
 }
